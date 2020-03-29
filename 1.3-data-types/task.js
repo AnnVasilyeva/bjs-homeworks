8ad3dd3e046a27d0e8276890923d989d6be34438
Ann Vasilyeva <skazkadreamanna@gmail.com>
2020-03-29 23:26:58 +0300

    third commit wrappers-for-primitives

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,34 @@
+'use strict';
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    let p = Number(percent);
+    let c = Number(contribution);
+    let a = Number(amount);
+    if (isNaN(p)) {
+	    return `Параметр процентная ставка содержит неправильное значение ${percent}`;
+    }
+    if (isNaN(c)) {
+	   return `Параметр сумму первоначального взноса содержит неправильное значение ${contribution}`; 
+    }
+    if (isNaN(a)) {
+	    return `Параметр сумма кредита содержит неправильное значение ${amount}`; 
+    }
+    
+    let s = a - c;
+	let today = new Date();
+	let n = (date.getFullYear() - today.getFullYear()) * 12 + (date.getMonth()) - (today.getMonth());
+	p = p / 12 / 100;
+	let payment = s * (p + p / ((Math.pow(1 + p, n)-1)));
+	
+	let totalMortgage = (payment * n);
+	
+    return Number(totalMortgage.toFixed(2));
+    
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+	if (typeof name === 'undefined' || name === null || name === "") {
+		return `Привет, мир! Меня зовут Аноним`;
+	}
+   let greeting = `Привет, мир! Меня зовут ${name}`;
+   return greeting;
 }
